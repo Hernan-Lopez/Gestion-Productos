179a95ada5624c7afea56a25ef53874efc7dd7b5
Hernan-Lopez <134668017+Hernan-Lopez@users.noreply.github.com>
2024-12-06 22:38:45 -0300

    refactor

--- src/main/java/com/hernan/gestionproductos/config/SecurityConfig.java
+++ src/main/java/com/hernan/gestionproductos/config/SecurityConfig.java
@@ -3,14 +3,9 @@ package com.hernan.gestionproductos.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -31,18 +26,6 @@ public class SecurityConfig {
 
 	    return http.build();
 	}
-
-//    @Bean
-//    UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-//        UserDetails user = User.builder()
-//            .username("a")
-//            .password(passwordEncoder.encode("a"))
-//            .roles("ADMIN")
-//            .build();
-//
-//        return new org.springframework.security.provisioning.InMemoryUserDetailsManager(user);
-//    }
-
 
     @Bean
     PasswordEncoder passwordEncoder() {
